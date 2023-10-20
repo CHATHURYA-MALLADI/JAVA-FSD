4b41d7eba26fe7413b86478e3ffe17600c3f72e9
CHATHURYA-MALLADI <113288344+CHATHURYA-MALLADI@users.noreply.github.com>
2023-10-20 15:49:17 +0530

    first commit

---
+++ SpringBootStarter/SpringBootStarter/src/main/java/com/ecommerce/controllers/EProductExceptionController.java
@@ -0,0 +1,20 @@
+package com.ecommerce.controllers;
+
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ecommerce.exceptions.ProductNotFoundException;
+
+@Controller
+public class EProductExceptionController {
+
+        @ExceptionHandler(value = ProductNotFoundException.class)
+           public ResponseEntity<Object> exception(ProductNotFoundException exception) {
+              return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+           }
+}
